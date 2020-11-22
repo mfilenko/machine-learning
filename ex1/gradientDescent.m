49a4a1cdb064574d3bf4de51f9662657b50db7f5
Max Filenko <contact@filenko.ms>
2020-11-22 19:33:59 +0100

    Part 3: Cost and Gradient descent

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,11 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    theta0 = theta(1) - alpha * (1 / m) * sum(theta(1) + theta(2) * X(:,2) - y);
+    theta1 = theta(2) - alpha * (1 / m) * sum((theta(1) + theta(2) * X(:,2) - y) .* X(:,2));
 
-
-
-
-
+    theta = [theta0; theta1];
 
     % ============================================================
 
